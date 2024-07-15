72352371b00922959bd7c86f38e3769f704b8975
Jaskaran Singh <jaskarans9650@gmail.com>
2024-07-15 19:04:55 +0530

    first commit

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,12 @@
+import StreamVideoProvider from "@/providers/StreamClientProvider";
+import React, { Children, ReactNode } from "react";
+
+const RootLayout = ({ children }: { children: ReactNode }) => {
+  return (
+    <main>
+      <StreamVideoProvider>{children}</StreamVideoProvider>
+    </main>
+  );
+};
+
+export default RootLayout;
