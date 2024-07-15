5030dfcd5ab0f496821bcb4d572fc1d104952d56
Jaskaran Singh <jaskarans9650@gmail.com>
2024-07-15 21:38:34 +0530

    sixth commit

--- app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -3,10 +3,7 @@ import { SignUp } from "@clerk/nextjs";
 export default function SignUpPage() {
   return (
     <main className="flex h-screen w-full items-center justify-center">
-      <SignUp
-        redirectUrl="https://zoom-workplace.vercel.app/"
-        afterSignUpUrl="https://zoom-workplace.vercel.app/"
-      />
+      <SignUp fallbackRedirectUrl="/" />
     </main>
   );
 }
