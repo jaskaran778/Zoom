2b55e7d4aa9a257d8782f522a3387357c61760a1
Jaskaran Singh <jaskarans9650@gmail.com>
2024-07-15 21:53:02 +0530

    seventh commit

--- app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -3,7 +3,7 @@ import { SignUp } from "@clerk/nextjs";
 export default function SignUpPage() {
   return (
     <main className="flex h-screen w-full items-center justify-center">
-      <SignUp fallbackRedirectUrl="/" />
+      <SignUp fallbackRedirectUrl="/" afterSignUpUrl="/" redirectUrl="/" />
     </main>
   );
 }
